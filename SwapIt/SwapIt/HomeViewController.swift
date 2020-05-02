23fbd2dddfc62eb9752418b58a6fb9ea3b2bba8c
Yao Yu <yao97ming@gmail.com>
2020-05-02 16:19:24 -0700

    exchange page

--- SwapIt/SwapIt/HomeViewController.swift
+++ SwapIt/SwapIt/HomeViewController.swift
@@ -8,12 +8,57 @@
 
 import UIKit
 
-class HomeViewController: UIViewController {
-
+class HomeViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    var currency:[String] = []
+    var values: [String] = []
+    @IBOutlet weak var tableview: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        tableview.delegate = self
+        tableview.dataSource = self
+        
+        let url = URL(string: "http://data.fixer.io/api/latest?access_key=950d9516c67b78c187ebee7055841efc")
+        
+        let task = URLSession.shared.dataTask(with: url!) { (data, response, error) in
+            if error != nil {
+                print("error")
+            }
+            else {
+                if let content = data {
+                    do {
+                        let myJson = try JSONSerialization.jsonObject(with: content, options: JSONSerialization.ReadingOptions.mutableContainers) as AnyObject
+                        print(myJson)
+                        if let rates = myJson["rates"] as? NSDictionary {
+                            for (key, value) in rates {
+                                self.currency.append((key as? String)!)
+                                self.values.append((value as? Double)!)
+                            }
+                            print(self.currency)
+                            print(self.values)
+                        }
+                    }
+                    catch {
+                        
+                    }
+                }
+            }
+        }
+        task.resume()
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return currency.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let currencies = currency[indexPath.row]
+        let value = values[indexPath.row]
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CurrencyCell") as! CurrencyCell
+        cell.currencyLabel.text = currencies
+        cell.valuesLabel.text = value
+        return cell
     }
     
 
